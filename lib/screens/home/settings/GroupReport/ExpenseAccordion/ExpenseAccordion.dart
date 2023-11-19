1b1ef14c9bd0ac75cc67bd9c76e8d51d9cb5326f
nevinmanoj <NEVINMANOJP@GMAIL.COM>
2023-11-20 00:20:47 +0530

    fixed group ac

--- lib/screens/home/settings/GroupReport/ExpenseAccordion/ExpenseAccordion.dart
+++ lib/screens/home/settings/GroupReport/ExpenseAccordion/ExpenseAccordion.dart
@@ -38,7 +38,9 @@ class _ExpenseAccordionState extends State<ExpenseAccordion> {
     }
 
     return Container(
+      padding: EdgeInsets.all(5),
       decoration: BoxDecoration(
+        color: Colors.white,
         border: Border.all(
           color: Colors.grey.withOpacity(0.3),
         ),
@@ -47,67 +49,64 @@ class _ExpenseAccordionState extends State<ExpenseAccordion> {
       // padding: EdgeInsets.all(16.0),
       margin: EdgeInsets.fromLTRB(wt * 0.05, 10, wt * 0.05, 0),
       child: SingleChildScrollView(
-        child: Column(
+        child: ExpansionPanelList(
+          elevation: 0,
+          expandedHeaderPadding: EdgeInsets.all(0),
+          expansionCallback: (int index, bool _isExpanded) {
+            setState(() {
+              // Update the selected option when an accordion is expanded
+              isExpanded = !_isExpanded;
+            });
+          },
           children: [
-            ExpansionPanelList(
-              elevation: 0,
-              expandedHeaderPadding: EdgeInsets.all(0),
-              expansionCallback: (int index, bool _isExpanded) {
-                setState(() {
-                  // Update the selected option when an accordion is expanded
-                  isExpanded = !_isExpanded;
-                });
-              },
-              children: [
-                ExpansionPanel(
-                  canTapOnHeader: true,
-                  headerBuilder: (BuildContext context, bool _isExpanded) {
-                    return const ListTile(
-                      title: Text(
-                        'Expenses',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                        ),
-                      ),
-                    );
-                  },
-                  body: SizedBox(
-                    height: ht * 0.5,
-                    child: ListView.builder(
-                      itemCount: groupedList.keys.length,
-                      itemBuilder: (context, i) {
-                        // print(i);
-                        // return null;
-                        String iDate = DateFormat.yMMMd()
-                            .format(DateTime.parse(widget.list[i]['date']))
-                            .toString();
-                        return Column(
-                          children: [
-                            Container(
-                              width: wt,
-                              color: const Color.fromARGB(255, 232, 232, 232),
-                              child: Padding(
-                                padding: const EdgeInsets.fromLTRB(10, 5, 0, 5),
-                                child: Text(
-                                  iDate,
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 18),
-                                ),
-                              ),
-                            ),
-                            for (var item in groupedList[iDate])
-                              itemWidget(
-                                  context: context, iDate: iDate, item: item)
-                          ],
-                        );
-                      },
+            ExpansionPanel(
+              canTapOnHeader: true,
+              headerBuilder: (BuildContext context, bool _isExpanded) {
+                return const ListTile(
+                  title: Text(
+                    'Expenses',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
                     ),
                   ),
-                  isExpanded: isExpanded,
+                );
+              },
+              body: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
                 ),
-              ],
+                height: ht * 0.5,
+                child: ListView.builder(
+                  itemCount: groupedList.keys.length,
+                  itemBuilder: (context, i) {
+                    // print(i);
+                    // return null;
+                    String iDate = DateFormat.yMMMd()
+                        .format(DateTime.parse(widget.list[i]['date']))
+                        .toString();
+                    return Column(
+                      children: [
+                        Container(
+                          width: wt,
+                          color: const Color.fromARGB(255, 232, 232, 232),
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(10, 5, 0, 5),
+                            child: Text(
+                              iDate,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 18),
+                            ),
+                          ),
+                        ),
+                        for (var item in groupedList[iDate])
+                          itemWidget(context: context, iDate: iDate, item: item)
+                      ],
+                    );
+                  },
+                ),
+              ),
+              isExpanded: isExpanded,
             ),
           ],
         ),
