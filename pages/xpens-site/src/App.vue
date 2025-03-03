2eff6bc1e1c58f68ab5a5cfc8d06ea5f27f3022c
nevinmanoj <NEVINMANOJP@GMAIL.COM>
2025-03-03 23:55:11 +0530

    website changes for versioning

--- pages/xpens-site/src/App.vue
+++ pages/xpens-site/src/App.vue
@@ -1,7 +1,31 @@
 
 <template>
+   <img :src="image" alt="Logo"  class="xpens-logo"/>
    <h1>Xpens </h1>
-   <a href="app-release.apk">Download App</a>
+   <div>v{{ config?.currentVersion }}</div>
+   <a class="download-link" href="app-release.apk">Download App</a>
 </template>
+
+
+<script >
+
+export default {
+  data() {
+    return {
+      config: null,
+      image:"release.png"
+    };
+  },
+  async mounted() {
+    try {
+      const response = await fetch("./version.json");
+      
+      this.config = await response.json();
+    } catch (error) {
+      console.error("Error loading JSON:", error);
+    }
+  }
+};
+</script>
 
   
